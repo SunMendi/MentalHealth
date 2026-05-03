03efcb21e061a4a99bb830cc10865bd5718177fb
mehedi <mahbub@evidentbd.com>
2026-05-03 12:54:03 +0600

    auth issue fixing

--- core/users/authentication.py
+++ core/users/authentication.py
@@ -16,6 +16,39 @@ def _fingerprint(value):
 
 
 class LoggingJWTAuthentication(JWTAuthentication):
+    def authenticate(self, request):
+        header = self.get_header(request)
+        if header is None:
+            return None
+
+        raw_token = self.get_raw_token(header)
+        if raw_token is None:
+            logger.warning(
+                "JWT header present but raw token missing | auth_header=%s | signing_key_fp=%s",
+                header[:32],
+                _fingerprint(settings.SIMPLE_JWT.get("SIGNING_KEY")),
+            )
+            return None
+
+        logger.info(
+            "Authenticating JWT | path=%s | method=%s | signing_key_fp=%s | token_prefix=%s",
+            getattr(request, "path", ""),
+            getattr(request, "method", ""),
+            _fingerprint(settings.SIMPLE_JWT.get("SIGNING_KEY")),
+            raw_token[:20].decode("utf-8", errors="ignore"),
+        )
+
+        validated_token = self.get_validated_token(raw_token)
+        user = self.get_user(validated_token)
+        logger.info(
+            "JWT accepted | path=%s | user_id=%s | claim_user_id=%s | signing_key_fp=%s",
+            getattr(request, "path", ""),
+            getattr(user, "id", None),
+            validated_token.get(settings.SIMPLE_JWT.get("USER_ID_CLAIM", "user_id")),
+            _fingerprint(settings.SIMPLE_JWT.get("SIGNING_KEY")),
+        )
+        return (user, validated_token)
+
     def get_validated_token(self, raw_token):
         try:
             return super().get_validated_token(raw_token)
